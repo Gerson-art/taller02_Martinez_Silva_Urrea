a4170e0c228fc36ba5b621d8873405c305bb11d1
sebas <sebasaraki@gmail.com>
2023-09-28 10:12:12 -0300

    Codigos seba

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,7 @@ public class Main {
         int opcion;
         Scanner sn = new Scanner(System.in);
         while (true) {
+            int [][] habitaciones = crearArreglo();
             System.out.println("Ingrese la contraseña: ");
             contra2 = sn.nextLine();
             if (contra.equals(contra2)){
@@ -14,12 +15,13 @@ public class Main {
                 System.out.println("2.- Confirmar reserva");
                 System.out.println("3.- Cancelar reserva");
                 System.out.println("4.- Mostrar Habitaciones");
-                System.out.println("5.- ");
+                System.out.println("5.- Reiniciar habitaciones");
                 System.out.println("5.- Salir");
                 System.out.println("Elija una opcion: ");
                 opcion = sn.nextInt();
                 switch (opcion) {
                     case 1:
+
 
                     case 2:
 
@@ -28,6 +30,10 @@ public class Main {
                     case 4:
 
                     case 5:
+                        reiniciarArreglo(habitaciones);
+                        System.out.println("Habitaciones reiniciadas");
+                        break;
+                    case 6:
                         System.exit(0);
                     default:
                         System.out.println("Opcion no valida");
@@ -41,6 +47,23 @@ public class Main {
         }
 
 
+    }
+
+    public static int[][] crearArreglo (){
+        int [][] habitaciones = new int[10][2];
+        for (int i = 0; i < habitaciones.length; i++) {
+            for (int j = 0; j < habitaciones[i].length; j++) {
+                habitaciones[i][j] = 0;
+            }
+        }
+        return habitaciones;
+    }
+    public static int [][] reiniciarArreglo(int [][] habitaciones){
+        for (int i = 0; i < habitaciones.length; i++) {
+            for (int j = 0; j < habitaciones[i].length; j++) {
+                habitaciones[i][j] = 0;
+            }
+        }
     }
 }
 

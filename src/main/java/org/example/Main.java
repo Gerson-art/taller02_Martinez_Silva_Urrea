5a963d00dc6123fec5ab6f8f1bab050fe0060beb
GersonUA <g.urrea02@ufromail.cl>
2023-09-28 10:00:59 -0300

    Actualizion codigo pagar habitacion e imprirmi boleta

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,37 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+import java.util.ArrayList;
+
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        int [][] Arr = new int [10][2];
+        for(int i=0; i< Arr.length; i++) {
+            for(int j=0; j< Arr[i].length; j++) {
+                Arr[i][j] = 0;
+            }
         }
     }
-}
+    public static void pagarHabitacion( int [][] arr ) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese la habitacion en la cual se hospedo");
+        int habitacion = sc.nextInt();
+        for(int i=0; i< arr.length; i++) {
+                if(arr[i][0] == habitacion) {
+                    arr[i][0] = 0;
+            }
+        }
+
+
+
+        }
+        public static void imprimirBoleta(int [][] arr, int habitacion) {
+            for (int i = 0; i < arr.length; i++) {
+
+                }
+            }
+        }
+    }
